92d6cad100b51b3187c82cdd912b57d4a2126e37
mokkeee <mokky.monkey.14@gmail.com>
2018-10-21 07:09:00 +0900

    First Commit

---
+++ TDDBC8th/PurelyImaginaryNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TDDBC8th
+{
+    public class PurelyImaginaryNumber
+    {
+        public readonly int imaginaryNumber;
+
+        public PurelyImaginaryNumber(int imaginaryNumber)
+        {
+            if( imaginaryNumber == 0 )
+            {
+                throw new ArgumentException("虚数0の純虚数は生成できません");
+            }
+            this.imaginaryNumber = imaginaryNumber;
+        }
+    }
+}
---
+++ TDDBC8th/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TDDBC8th
+{
+    [TestClass]
+    public class 純虚数Test
+    {
+        [TestClass]
+        public class 純虚数の生成
+        {
+            [TestMethod]
+            public void 虚部１の純虚数が生成できること() 
+            {
+                var pin = new PurelyImaginaryNumber(1);
+                Assert.AreEqual(1, pin.imaginaryNumber);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentException))]
+            public void 虚部0の純虚数が生成できないこと()
+            {
+                new PurelyImaginaryNumber(0);
+            }
+        }
+    } 
+}
